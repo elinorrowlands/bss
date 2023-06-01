39b00d22bd9a6e196e4dca07b02981b3cf624728
Charles Matthews <charles0matthews@gmail.com>
2023-06-01 10:34:10 -0400

    first

---
+++ compile.js
@@ -0,0 +1,20 @@
+import pug from 'pug';
+import fs from 'fs';
+
+let pugFiles = [
+    'index',
+];
+
+let pugSourceFolder = './src/pug';
+let pugOutputFolder = './docs'
+
+for (let file of pugFiles){
+    var htmlOutput = pug.render(fs.readFileSync(
+        `${pugSourceFolder}/${file}.pug`, "utf-8"), {
+            filename: `${pugSourceFolder}/${file}.pug`,
+            pretty: true
+        }
+    )
+    
+    fs.writeFileSync(`${pugOutputFolder}/${file}.html`, htmlOutput, "utf-8");
+}
