a2164f6f033cb9198fc665dd3f2cd2bac28925c5
Charles Matthews <charles0matthews@gmail.com>
2023-06-27 11:59:51 -0400

    Update text.js

--- docs/elements/text/text.js
+++ docs/elements/text/text.js
@@ -1,9 +1,14 @@
-const echo = new Tone.FeedbackDelay('1n', 0.5).toDestination();
+const filter = new Tone.Filter(200, 'lowpass').toDestination();
+const echo = new Tone.FeedbackDelay('1n', 0.5).connect(filter);
 const player = new Tone.Player('./waters_excerpt.mp3').toDestination();
+const backdrop = new Tone.Player('./backdrop.mp3').connect(filter).toDestination();
+backdrop.loop = true;
+backdrop.volume.value = '-12';
 player.connect(echo);
 const newCaption = ["00:00:06.420,00:00:08.160\nwhere is the water's edge?", '00:00:11.700,00:00:13.380\nin the wide expanses', '00:00:15.030,00:00:16.950\nor the half filled bathtub', '00:00:19.260,00:00:20.100\nin the river', '00:00:20.580,00:00:21.840\nor the boiling kettle', '00:00:22.770,00:00:23.760\nin the steam', '00:00:23.880,00:00:24.750\nor the snow', '00:00:25.140,00:00:28.170\nor the rainbow mist of a summer hose'];
 let captionObject = syncCC.splitCaptions(newCaption);
 console.log(captionObject);
+let playFlag = false;
 let justText = captionObject.map(text => text.content[0]);
 captionObject.forEach((text,i) => {
     let textElement = document.createElement('div');
@@ -15,7 +20,8 @@ captionObject.forEach((text,i) => {
         fontSize: i==0? '50px': (20+Math.random()*30)+'px',
         userSelect: 'none',
         cursor:'pointer',
-        color:`rgba(${colourValue},${colourValue},${colourValue},1)`
+        color:`rgba(${colourValue},${colourValue},${colourValue},1)`,
+        opacity: i==0? 1:0.1
     })
     textElement.id = `text_${i}`;
     textElement.innerHTML = text.content[0];
@@ -56,6 +62,11 @@ const Pickup = (e) =>{
     
     // let id = getNoteFromId(element.id);
     if(type == 'start' || type == 'enter'){
+        if(!playFlag){
+            backdrop.start();
+            playFlag = true;
+        }
+        filter.frequency.rampTo((y)+80, 0.5);
         element.classList.add('active');
         document.body.style.filter = `hue-rotate(${element.style.left}deg)`;
         player.start(Tone.now(), text.startS, text.endS-text.startS);
@@ -64,6 +75,7 @@ const Pickup = (e) =>{
         console.log(element.style.top, element.style.left)
         // synth.triggerAttack(id);
     } else if (type == 'end' || type == 'leave'){
+        filter.frequency.rampTo(200, 0.5);
         element.classList.remove('active');
         document.querySelector(`#text_${(element.id.split('_')[1] + 1) % (Object.keys(captionObject).length - 1)}`).style.left = (5+Math.random()*70)+'%';
         document.querySelector(`#text_${(element.id.split('_')[1] + 1) % (Object.keys(captionObject).length - 1)}`).style.top = (5+Math.random()*70)+'%';
@@ -71,6 +83,7 @@ const Pickup = (e) =>{
         document.body.style.filter = `hue-rotate(0deg)`;
     } else if(type == 'move'){
         document.body.style.filter = `hue-rotate(${element.x}deg)`;
+        filter.frequency.rampTo((y)+ 200, 0.5);
         element.style.left = x+'px';
         element.style.top = y+'px';
         document.querySelector(`#text_${(element.id.split('_')[1] + 2) % (Object.keys(captionObject).length - 1)}`).style.left = (5+Math.random()*70)+'%';
