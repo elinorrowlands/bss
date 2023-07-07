b5404f756cc804dcd7af4cf5e4dcab474517d97f
Charles Matthews <charles0matthews@gmail.com>
2023-07-07 17:40:42 -0400

    loading message

--- docs/elements/text/text.js
+++ docs/elements/text/text.js
@@ -1,10 +1,7 @@
 // todo: fix issue with loading time
 
-function ready(){
-    console.log('ready');
-}
 
-Tone.loaded().then(ready);
+
 
 const filter = new Tone.Filter(200, 'lowpass').toDestination();
 const echo = new Tone.FeedbackDelay('1n', 0.5).connect(filter);
@@ -12,7 +9,7 @@ const player = new Tone.Player('./waters_excerpt.mp3').toDestination();
 const backdrop = new Tone.Player('./backdrop.mp3').connect(filter).toDestination();
 const loadCount = 0;
 
-console.log('testing load time', player.buffer.loaded, backdrop.buffer.loaded)
+Tone.loaded().then(ready);
 
 backdrop.loop = true;
 backdrop.volume.value = '-12';
@@ -25,79 +22,91 @@ let playFlag = false;
 
 // let justText = captionObject.map(text => text.content[0]);
 
-captionObject.forEach((text,i) => {
-    let textElement = document.createElement('div');
-    let colourValue = ((12+i)*10)+(Math.random()*(255-((12+i)*10)));
+function ready(){
+    console.log('running...');
+    document.querySelectorAll('.loading').forEach(element => {
+        element.style.opacity = 0;
+        setTimeout(() => {
+            element.style.display = 'none';
+        }
+        , 1000);
+    });
 
-    Object.assign(textElement.style,{
-        position: 'absolute',
-        top: i==0 ? '45%': (5+Math.random()*80)+'%',
-        left: i==0 ? '45%':(5+Math.random()*70)+'%',
-        fontSize: i==0 ? '50px': (20+Math.random()*30)+'px',
-        userSelect: 'none',
-        cursor:'pointer',
-        color:`rgba(${colourValue},${colourValue},${colourValue},1)`,
-        opacity: i==0 ? 1:0.1
+    captionObject.forEach((text,i) => {
+        let textElement = document.createElement('div');
+        let colourValue = ((12+i)*10)+(Math.random()*(255-((12+i)*10)));
+    
+        Object.assign(textElement.style,{
+            position: 'absolute',
+            top: i==0 ? '45%': (5+Math.random()*80)+'%',
+            left: i==0 ? '45%':(5+Math.random()*70)+'%',
+            fontSize: i==0 ? '50px': (20+Math.random()*30)+'px',
+            userSelect: 'none',
+            cursor:'pointer',
+            color:`rgba(${colourValue},${colourValue},${colourValue},1)`,
+            opacity: i==0 ? 1:0.1
+        });
+        
+        textElement.id = `text_${i}`;
+        textElement.innerHTML = text.content[0];
+        textElement.classList.add('text');
+    
+        captionObject.element = textElement;
+        document.body.appendChild(textElement);
     });
     
-    textElement.id = `text_${i}`;
-    textElement.innerHTML = text.content[0];
-    textElement.classList.add('text');
-
-    captionObject.element = textElement;
-    document.body.appendChild(textElement);
-});
-
-touch.setAction('.text');
-
-const Pickup = (e) =>{
-    const preset = {
-        opacity:{
-            start:0.5, enter:0.5, end:1, move:0.5, leave:1
+    touch.setAction('.text');
+    
+    const Pickup = (e) =>{
+        const preset = {
+            opacity:{
+                start:0.5, enter:0.5, end:1, move:0.5, leave:1
+            }
         }
+        
+        let { element, type, x, y } = e.detail;
+        
+        let target = element;
+        
+        let text = captionObject[element.id.split('_')[1]];
+        
+        if(type == 'start' || type == 'enter'){
+            if(!playFlag){
+                backdrop.start();
+                playFlag = true;
+            }
+    
+            filter.frequency.rampTo((y)+80, 0.5);
+            element.classList.add('active');
+            document.body.style.filter = `hue-rotate(${element.style.left}deg)`;
+            player.start(Tone.now(), text.startS, text.endS-text.startS);
+            element.style.left = x+'px';
+            element.style.top = y+'px';
+            console.log(element.style.top, element.style.left)
+            
+        } else if (type == 'end' || type == 'leave'){
+    
+            filter.frequency.rampTo(200, 0.5);
+            element.classList.remove('active');
+            document.querySelector(`#text_${(element.id.split('_')[1] + 1) % (Object.keys(captionObject).length - 1)}`).style.left = (5+Math.random()*70)+'%';
+            document.querySelector(`#text_${(element.id.split('_')[1] + 1) % (Object.keys(captionObject).length - 1)}`).style.top = (5+Math.random()*70)+'%';
+            
+            document.body.style.filter = `hue-rotate(0deg)`;
+    
+        } else if(type == 'move'){
+    
+            document.body.style.filter = `hue-rotate(${element.x}deg)`;
+            filter.frequency.rampTo((y)+ 200, 0.5);
+            element.style.left = x+'px';
+            element.style.top = y+'px';
+            document.querySelector(`#text_${(element.id.split('_')[1] + 2) % (Object.keys(captionObject).length - 1)}`).style.left = (5+Math.random()*70)+'%';
+            document.querySelector(`#text_${(element.id.split('_')[1] + 2) % (Object.keys(captionObject).length - 1)}`).style.top = (5+Math.random()*70)+'%';
+    
+        }
+        
+        target.style.opacity = preset.opacity[type];
     }
     
-    let { element, type, x, y } = e.detail;
-    
-    let target = element;
-    
-    let text = captionObject[element.id.split('_')[1]];
-    
-    if(type == 'start' || type == 'enter'){
-        if(!playFlag){
-            backdrop.start();
-            playFlag = true;
-        }
-
-        filter.frequency.rampTo((y)+80, 0.5);
-        element.classList.add('active');
-        document.body.style.filter = `hue-rotate(${element.style.left}deg)`;
-        player.start(Tone.now(), text.startS, text.endS-text.startS);
-        element.style.left = x+'px';
-        element.style.top = y+'px';
-        console.log(element.style.top, element.style.left)
-        
-    } else if (type == 'end' || type == 'leave'){
-
-        filter.frequency.rampTo(200, 0.5);
-        element.classList.remove('active');
-        document.querySelector(`#text_${(element.id.split('_')[1] + 1) % (Object.keys(captionObject).length - 1)}`).style.left = (5+Math.random()*70)+'%';
-        document.querySelector(`#text_${(element.id.split('_')[1] + 1) % (Object.keys(captionObject).length - 1)}`).style.top = (5+Math.random()*70)+'%';
-        
-        document.body.style.filter = `hue-rotate(0deg)`;
-
-    } else if(type == 'move'){
-
-        document.body.style.filter = `hue-rotate(${element.x}deg)`;
-        filter.frequency.rampTo((y)+ 200, 0.5);
-        element.style.left = x+'px';
-        element.style.top = y+'px';
-        document.querySelector(`#text_${(element.id.split('_')[1] + 2) % (Object.keys(captionObject).length - 1)}`).style.left = (5+Math.random()*70)+'%';
-        document.querySelector(`#text_${(element.id.split('_')[1] + 2) % (Object.keys(captionObject).length - 1)}`).style.top = (5+Math.random()*70)+'%';
-
-    }
-    
-    target.style.opacity = preset.opacity[type];
+    document.addEventListener('touch-pickup',(e)=>Pickup(e));
 }
 
-document.addEventListener('touch-pickup',(e)=>Pickup(e));
