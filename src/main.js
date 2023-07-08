da45fedc5ea982f29d10fffbd7ecc60f822cf714
Charles Matthews <charles0matthews@gmail.com>
2023-07-08 17:52:06 -0400

    qr codes now generated dynamically
    but have lost internal styling...

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import {srtToSbv} from '@matthewscharles/cm-toolbox';
+import {srtToSbv, generateQR} from '@matthewscharles/cm-toolbox';
 import SyncCC from 'sync-cc';
 import MultitouchMapper from '@matthewscharles/multitouch-mapper'
 window.syncCC = new SyncCC();
-
+window.generateQR = generateQR;
 window.srtToSbv = srtToSbv;
 window.touch = new MultitouchMapper();
