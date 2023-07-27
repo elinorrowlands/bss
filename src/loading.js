340853cb2ccab682e840cbcff6d8bf0f13eecf0e
Charles Matthews <charles0matthews@gmail.com>
2023-07-26 22:27:40 -0400

    Update loading.js

--- src/loading.js
+++ src/loading.js
@@ -1,8 +1,20 @@
 // create html element (assign style in css)
 // set timeout 
 
+function loaded(){
+    // todo: add load status message for screen readers
+    document.querySelectorAll('.loading').forEach(element => {
+        clearInterval(loadBlink);
+        element.style.opacity = 0;
+        setTimeout(() => {
+            element.style.display = 'none';
+        }, 1000);
+    });
+}
+
 export default function createLoadingIndicator(){
     let loading = document.createElement('div');
     loading.classList.add('loading');
     loading.innerHTML = 'Loading...';
+    document.body.appendChild(loading);
 }
