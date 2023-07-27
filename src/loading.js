7534a5af5a3ede832ad5ed9d56ff08f50242080f
Charles Matthews <charles0matthews@gmail.com>
2023-07-27 07:53:14 -0400

    loading indicator

--- src/loading.js
+++ src/loading.js
@@ -1,6 +1,26 @@
 // create html element (assign style in css)
 // set timeout 
 
+let loadingIndicator = {
+    blinkCount: 0,
+    createLoadingIndicator(){
+        let loading = document.createElement('div');
+        loading.classList.add('loading');
+        loading.innerHTML = 'Loading...';
+        document.body.appendChild(loading);
+    },
+    init(interval = 5000){
+        this.createLoadingIndicator();
+        setInterval(this.blink, interval);
+    },
+    blink(){
+        document.querySelectorAll('.loadMsg')[0].style.opacity=loadBlinkCount%2 ? 0.8:1;
+        document.querySelectorAll('.loading')[0].style.backgroundColor=loadBlinkCount%2 ? 'navy':'#0081ff';
+        document.querySelectorAll('.loadMsg')[0].style.backgroundColor=loadBlinkCount%2 ? 'navy':'#0081ff';
+        this.blinkCount++;
+    }
+}
+
 function loaded(){
     // todo: add load status message for screen readers
     document.querySelectorAll('.loading').forEach(element => {
@@ -12,9 +32,5 @@ function loaded(){
     });
 }
 
-export default function createLoadingIndicator(){
-    let loading = document.createElement('div');
-    loading.classList.add('loading');
-    loading.innerHTML = 'Loading...';
-    document.body.appendChild(loading);
-}
+
+export { loadingIndicator, loaded }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import {srtToSbv, generateQR} from '@matthewscharles/cm-toolbox';
 import SyncCC from 'sync-cc';
 import MultitouchMapper from '@matthewscharles/multitouch-mapper'
 import {mapLoad} from './map_interaction.js';
-import createLoadingIndicator from './loading.js';
+import {loadingIndicator} from './loading.js';
 window.syncCC = new SyncCC();
 window.generateQR = generateQR;
 window.srtToSbv = srtToSbv;
