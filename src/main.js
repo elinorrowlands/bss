40e9e70d4dfdd107ded62f7a4349ff4b9df6379c
Charles Matthews <charles0matthews@gmail.com>
2023-07-28 14:27:47 -0400

    installed screen instrument package

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import SyncCC from 'sync-cc';
 import MultitouchMapper from '@matthewscharles/multitouch-mapper'
 import {mapLoad} from './map_interaction.js';
 import {loadingIndicator} from './loading.js';
+import { ScreenInstrument } from '@matthewscharles/screen-instrument';
 window.syncCC = new SyncCC();
 window.generateQR = generateQR;
 window.srtToSbv = srtToSbv;
 window.touch = new MultitouchMapper();
+window.instrument = new ScreenInstrument();
 window.mapLoad = mapLoad;
