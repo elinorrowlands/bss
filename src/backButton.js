b055a862e0b70d71a491f9452b28d7fbb0129d83
Charles Matthews <charles0matthews@gmail.com>
2023-08-07 11:44:13 -0400

    fixed back button, points to main site

--- src/backButton.js
+++ src/backButton.js
@@ -6,7 +6,7 @@ function addBackButton(){
     var backButton = document.createElement("button");
     backButton.innerHTML = "Biodivergent Sites and Sounds";
     // backButton.setAttribute("onclick", "window.history.back()");
-    backButton.setAttribute("onclick", "window.location = https://elinorrowlands.github.io/biodivergent-sites-and-sounds/");
+    backButton.setAttribute("onclick", "window.location = 'https://elinorrowlands.github.io/bss/'");
     backButton.id = "backButton";
     backButton.classList.add('button', 'nav__back')
     document.body.appendChild(backButton);
