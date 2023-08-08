5ed7488e488b25534408045242d573d5f4d2b992
Charles Matthews <charles0matthews@gmail.com>
2023-08-07 21:43:35 -0400

    back button added to map

--- src/map.js
+++ src/map.js
@@ -4,9 +4,12 @@ import MultitouchMapper from '@matthewscharles/multitouch-mapper'
 
 import { mapLoad } from './map_interaction.js';
 import { loadingIndicator } from './loading.js';
+import addBackButton from './backButton.js';
 
 window.syncCC = new SyncCC();
 window.generateQR = generateQR;
 window.srtToSbv = srtToSbv;
 window.touch = new MultitouchMapper();
 window.mapLoad = mapLoad;
+window.loadingIndicator = loadingIndicator;
+window.addBackButton = addBackButton;
