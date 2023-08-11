ebfc664a84f6a3ed7d7b0d12c856970d62ea4bc0
Charles Matthews <charles0matthews@gmail.com>
2023-08-11 18:40:02 -0400

    add option to return to map from url search params

--- src/backButton.js
+++ src/backButton.js
@@ -6,7 +6,12 @@ function addBackButton(){
     var backButton = document.createElement("button");
     backButton.innerHTML = "Biodivergent Sites and Sounds";
     // backButton.setAttribute("onclick", "window.history.back()");
-    backButton.setAttribute("onclick", "window.location = 'https://elinorrowlands.github.io/bss/'");
+    let urlParams = new URLSearchParams(window.location.search);
+    if(urlParams.has('from')){
+        let home = urlParams.get('from');
+    }
+    let homePage = urlParams.get('from') == 'map' ? 'map2' : 'index';
+    backButton.setAttribute("onclick",`window.location = 'https://elinorrowlands.github.io/bss/${homePage}.html'`);
     backButton.id = "backButton";
     backButton.classList.add('button', 'nav__back')
     document.body.appendChild(backButton);
