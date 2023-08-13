a5cacb4ddc1806c67fdc513afd8221a06d37a18d
Charles Matthews <charles0matthews@gmail.com>
2023-08-12 21:03:32 -0400

    mix modifications

--- docs/elements/mix/mix.js
+++ docs/elements/mix/mix.js
@@ -1,4 +1,22 @@
+let meterInterval;
+let startButton = document.querySelector("#start");
+let playFlag = false;
 
+let meterUpdate = function(){
+    mix.meters.forEach((meter,i) => {
+        let level = Tone.dbToGain(meter.getValue());
+        document.querySelector(`#circle_${i}`).style.r = `${Math.floor(level*10000)}`;
+        // document.querySelector(`#circle_${i}`).style.ry = `${Math.floor(level*100000)}`;
+    })
+}
+
+function setMeters(state){
+    if(state){
+        meterInterval = setInterval(meterUpdate, 100);
+    } else {
+        clearInterval(meterInterval);
+    }
+}
 
 function loaded(){
     // todo: add load status message for screen readers
@@ -12,6 +30,13 @@ function loaded(){
 
 function start(){
     loaded();
+    sounds.forEach((sound,i) => {
+        durations[i] = sound.buffer.duration;
+    })
+
+    document.querySelectorAll('.waveform').forEach((element,i) => {
+        heights[i] = element.clientHeight;
+    })
 }
 
 window.mix = {
@@ -28,10 +53,15 @@ mix.reverbChannel.connect(mix.filter);
 mix.filter.connect(mix.reverb);
 mix.crossFader.toDestination();
 
-let sounds = [
+window.sounds = [
     new Tone.Player("channels_L.mp3"),
     new Tone.Player("channels_R.mp3")
 ];
+
+window.durations = [0,0];
+window.heights = [0,0];
+
+// is this still relevant? or now redundant?
 
 Tone.loaded().then(start);
 
@@ -39,38 +69,24 @@ sounds.forEach((sound,i) => {
     sound.connect(mix.channels[i]);
     sound.connect(mix.meters[i]);
     sound.connect(mix.reverbChannel);
+
     mix.channels[i].pan.value = 0.5 - i;
     mix.channels[i].connect(i === 0 ? mix.crossFader.a : mix.crossFader.b);
 });
 
-let startButton = document.querySelector("#start");
-let playFlag = false;
-
 startButton.addEventListener("click", () => {
     playFlag = !playFlag;
+    crossFaderUpdate({target:{value:0.5}});
     setMeters(playFlag);
+
     startButton.innerHTML = playFlag ? "STOP" : "START";
+
     sounds.forEach(sound => {
         sound[playFlag ? 'start' : 'stop']()
     })
 })
 
-let meterUpdate = function(){
-    mix.meters.forEach((meter,i) => {
-        let level = Tone.dbToGain(meter.getValue());
-        document.querySelector(`#circle_${i}`).style.r = `${Math.floor(level*1000)}%`;
-    })
-}
 
-let meterInterval;
-
-function setMeters(state){
-    if(state){
-        meterInterval = setInterval(meterUpdate, 100);
-    } else {
-        clearInterval(meterInterval);
-    }
-}
 
 import crossFaderUpdate from './crossFader.js';
 document.querySelector('#crossFader').addEventListener("input", crossFaderUpdate);
