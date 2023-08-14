80e0bd66d246744a206a7506690c2004907adaf4
Charles Matthews <charles0matthews@gmail.com>
2023-08-14 12:01:36 -0400

    stylise waveforms

--- docs/elements/mix/crossFader.js
+++ docs/elements/mix/crossFader.js
@@ -8,9 +8,11 @@ const crossFaderUpdate = (e) => {
     let percentage = value * 50;
     let leftValue = (percentage/2);
     let rightValue = 100-(percentage/2);
-    document.querySelector('main').style.background = `linear-gradient(90deg, rgba(249,212,35,0.5) ${leftValue}%, rgba(32,42,68,0.4) ${rightValue}%)`;
+    document.querySelector('main').style.background = `linear-gradient(90deg, rgba(249,212,35,0.2) ${leftValue}%, rgba(32,42,68,0.2) ${rightValue}%)`;
     mix.crossFader.fade.rampTo(value, 0.1, Tone.now());
-
+    document.querySelectorAll('.waveform').forEach((x,i)=>{
+        x.style.opacity = 0.2 + Math.abs(1-i-value)*0.8;
+    })
     mix.channels.forEach((channel,i) => {
         let balance = 0.5*(i-value);
         channel.pan.rampTo(balance, 0.1, Tone.now())
