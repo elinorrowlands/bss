6230bdba1e8332e60a64facf24e899ae7177a3f0
Charles Matthews <charles0matthews@gmail.com>
2023-08-18 09:45:49 -0400

    following up on navigation

--- src/backButton.js
+++ src/backButton.js
@@ -3,16 +3,28 @@
  */
 
 function addBackButton(){
-    var backButton = document.createElement("button");
-    backButton.innerHTML = "Biodivergent Sites and Sounds";
-    // backButton.setAttribute("onclick", "window.history.back()");
     let urlParams = new URLSearchParams(window.location.search);
+    
     if(urlParams.has('from')){
         let home = urlParams.get('from');
     }
+    
     if(urlParams.has('standalone')){
         return;
     }
+    
+    // check if an equivalent to the back button already exists, return if so
+    
+    if(document.querySelector(".nav__back")){   
+        return;
+    }
+    
+    var backButton = document.createElement("button");
+    backButton.innerHTML = "Biodivergent Sites and Sounds";
+    // backButton.setAttribute("onclick", "window.history.back()");
+    
+    
+    
     let homePage = urlParams.get('from') == 'map' ? 'map2' : 'index';
     backButton.setAttribute("onclick",`window.location = 'https://elinorrowlands.github.io/bss/${homePage}.html'`);
     backButton.id = "backButton";
