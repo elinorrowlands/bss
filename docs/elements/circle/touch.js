8a34756268c1a1e331f02a286eed1a5cd9132c8a
Charles Matthews <charles0matthews@gmail.com>
2023-09-01 10:52:52 -0400

    add some faint circles

--- docs/elements/circle/touch.js
+++ docs/elements/circle/touch.js
@@ -29,22 +29,27 @@ const Pickup = (e) =>{
     if(query=='.guide'){
         let linkedId = element.dataset.linked;
         let amount = (type == 'start' || type == 'enter' || type=='move') ? 200 : 0;
+        // if(sounds[id] && type=='move'){
+        //     sounds[id].reverse = true;
+        // }
         document.querySelector(`#${linkedId}`).style.filter=`grayScale(${amount}%) brightness(${amount}%)`;
         sounds.notch2.Q.rampTo(amount/100, 0.1);
     }
     
     if(type == 'start' || type == 'enter'){
         if(sounds[id]){
-            sounds[id].volume.rampTo(-12,0.3);
+            sounds[id].volume.rampTo(-12,0.7);
             sounds.loop.volume.rampTo(-30,1);
         }
     } else if (type == 'end' || type == 'leave'){
         if(sounds[id]){
             sounds[id].volume.rampTo(-Infinity,1);
             sounds.loop.volume.rampTo(-12,3);
+            // sounds[id].reverse = false;
         }
         document.body.style.filter = `hue-rotate(0deg)`;
     } else if(type == 'move'){
+      
         document.body.style.filter = `hue-rotate(${element.x}deg)`;
         let notchValue = 8000 * ((y)/range.y);
         sounds.notch.frequency.rampTo(notchValue, 0.1);
