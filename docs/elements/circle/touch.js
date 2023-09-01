e4069ec0ed74d320b2297671c0e681d8ed5681ee
Charles Matthews <charles0matthews@gmail.com>
2023-09-01 09:25:24 -0400

    fix some can elements

--- docs/elements/circle/touch.js
+++ docs/elements/circle/touch.js
@@ -17,6 +17,8 @@
 if(!window.multitouchMapper) multitouchMapper = window.touch;
 
 multitouchMapper.setAction('.visual')
+    .setAction('.canBG')
+    .setAction('.canvas')
 
 function getNoteFromId(id){
     return Tone.Frequency(1*(parseFloat(id.split('_')[1])%12)+72, 'midi').toFrequency()
@@ -32,35 +34,21 @@ function getTarget(id){
  */
 
 const Pickup = (e) =>{
-    const preset = {
-        transition:{
-            start:0.5, enter:0.5, end:1, move:-1, leave:2
-        },
-        opacity:{
-            start:0.5, enter:0.5, end:1, move:0.5, leave:1
-        }
-    }
-    
     let { element, type } = e.detail;
-    //todo: debounce
     let target = getTarget(element.id);
     let value = preset.transition[type];
     
-    if(value>-1){
-        target.style.transition = `all ${value}s ease`;
-    }
-    
     let id = getNoteFromId(element.id);
     if(type == 'start' || type == 'enter'){
-        synth.triggerAttack(id);
+        
     } else if (type == 'end' || type == 'leave'){
-        synth.triggerRelease(id);
+        
         document.body.style.filter = `hue-rotate(0deg)`;
     } else if(type == 'move'){
         document.body.style.filter = `hue-rotate(${element.x}deg)`;
     }
     
-    target.style.opacity = preset.opacity[type];
+    
 }
 
 document.addEventListener('touch-pickup',(e)=>Pickup(e));
