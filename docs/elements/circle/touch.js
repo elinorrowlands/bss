ab3d405c83e9a9d326dc1ab4cccb351915dc0049
Charles Matthews <charles0matthews@gmail.com>
2023-09-01 13:03:03 -0400

    tweaking can

--- docs/elements/circle/touch.js
+++ docs/elements/circle/touch.js
@@ -26,23 +26,23 @@ let counter1 = 0;
 multitouchMapper.depth = 3;
 const Pickup = (e) =>{
     let { element, type, query} = e.detail;
-    // console.log(type, element.id);
     console.log(counter1)
     
     let {y, x, range} = e.detail.relative;
-    let id = element.parentElement.id;
+    // let id = element.parentElement.id;
+    let id = element.dataset.sound;
     if(query=='.guide'){
         let linkedId = element.dataset.linked;
         let amount = (type == 'start' || type == 'enter' || type=='move') ? 200 : 0;
         document.querySelector(`#${linkedId}`).style.filter=`grayScale(${amount}%) brightness(${amount}%)`;
         // sounds.notch2.Q.rampTo(amount/200, 0.1);
     } else if(element.dataset.loop){
-        sounds[element.dataset.loop].volume.rampTo(-20,1);
+        // sounds[element.dataset.loop].volume.rampTo(-20,1);
     }
     
     if(type == 'start' || type == 'enter'){
         if(sounds[id]){
-            sounds[id].volume.rampTo(-12,0.7);
+            sounds[id].volume.rampTo(-12,1);
             sounds.loop.volume.rampTo(-30,1);
         }
         if(query == '.circ'){
@@ -62,15 +62,19 @@ const Pickup = (e) =>{
         document.body.style.filter = `hue-rotate(0deg)`;
     } else if(type == 'move'){
         sounds.can.playbackRate = (x/range.x)*2;
-        document.body.style.filter = `hue-rotate(${element.x}deg)`;
-        if(sounds[id])console.log(id, sounds[id].volume.value)
-        let notchValue = 8000 * ((y)/range.y);
-        sounds.notch.frequency.rampTo(notchValue, 0.1);
-        notchValue = 8000 * ((x)/range.x);
+        // document.body.style.filter = `hue-rotate(${element.x}deg)`;
+        if(sounds[id]){
+            // sounds[id].volume.rampTo(-12,1);
+        }
+        let notchValue = 4000+(4000 * ((y)/range.y));
+        sounds.notch.frequency.rampTo(notchValue, 1);
+        notchValue = 4000+(4000 * ((x)/range.x));
         sounds.notch2.frequency.rampTo(notchValue, 1);
-        console.log(e.detail.delta);
-        console.log(sounds.echo.delayTime.value);
-        sounds.echo.delayTime.rampTo(sounds.echo.delayTime.value + e.detail.delta.x/1000, 0.1);
+        // console.log(e.detail.delta);
+        // console.log(sounds.echo.delayTime.value);
+        if(e.detail.delta.x>1.5)sounds.echo.delayTime.rampTo(sounds.echo.delayTime.value + e.detail.delta.x/100, 1);
+        // console.log(e.detail.delta.x*100, 'delta')
+        // document.querySelector('#content').style.filter = `brightness(${Math.floor(e.detail.delta.x *300)}%)`;
     }
 }
 
