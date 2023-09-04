644fc976c9cd479a708eca7cca1a5951e18f2ce5
Charles Matthews <charles0matthews@gmail.com>
2023-09-04 19:25:53 -0400

    tidying up from troubleshooting

--- docs/elements/circle/touch.js
+++ docs/elements/circle/touch.js
@@ -22,11 +22,10 @@ multitouchMapper
     .setAction('#canvas')
     .setAction('.guide')
     .setAction('.circ')
-let counter1 = 0;
+
 multitouchMapper.depth = 3;
 const Pickup = (e) =>{
     let { element, type, query} = e.detail;
-    console.log(counter1)
     
     let {y, x, range} = e.detail.relative;
     let id = element.dataset.sound;
@@ -45,34 +44,39 @@ const Pickup = (e) =>{
             sounds.loop.volume.rampTo(-30,1);
             sounds.loop.reverse = true;
         }
+        
         if(query == '.circ'){
             sounds.can.start();
             sounds.can.reverse=false;
         }
     } else if (type == 'end' || type == 'leave'){
-        counter1++;
+        
         if(sounds[id]){
             sounds[id].volume.rampTo(-Infinity,8);
             sounds.loop.volume.rampTo(-12,3);
             sounds.loop.reverse = false;
         }
+        
         if(query == '.circ'){
             if(sounds.can.state =='stopped') sounds.can.start();
             sounds.can.reverse = true;
         }
         document.body.style.filter = `hue-rotate(0deg)`;
     } else if(type == 'move'){
+        
         sounds.can.playbackRate = (x/range.x)*2;
         if(sounds[id]){
             // sounds[id].volume.rampTo(-12,1);
-            
         }
+        
         let notchValue = 4000+(4000 * ((y)/range.y));
         sounds.notch.frequency.rampTo(notchValue, 1);
         notchValue = 4000+(4000 * ((x)/range.x));
         sounds.notch2.frequency.rampTo(notchValue, 1);
 
-        if(e.detail.delta.x>10) sounds.echo.delayTime.rampTo(sounds.echo.delayTime.value + e.detail.delta.x/1000, 1);
+        if(e.detail.delta.x>10) {
+            sounds.echo.delayTime.rampTo(sounds.echo.delayTime.value + e.detail.delta.x/1000, 1)
+        };
     }
 }
 
