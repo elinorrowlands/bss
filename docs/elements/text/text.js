179cc0f92dada66c8d751715a67b448c01e698d6
Charles Matthews <charles0matthews@gmail.com>
2023-09-09 18:13:35 -0400

    modify text demo

--- docs/elements/text/text.js
+++ docs/elements/text/text.js
@@ -2,6 +2,9 @@ const filter = new Tone.Filter(200, 'lowpass').toDestination();
 const echo = new Tone.FeedbackDelay('1n', 0.5).connect(filter);
 const player = new Tone.Player('./waters_excerpt.mp3').toDestination();
 const backdrop = new Tone.Player('./backdrop.mp3').connect(filter).toDestination();
+backdrop.loop = true;
+backdrop.volume.value = '-12';
+player.connect(echo);
 
 /**
  * Loading screen to break out
@@ -14,19 +17,15 @@ const loadBlink = () => {
     document.querySelectorAll('.loadMsg')[0].style.backgroundColor=loadBlinkCount%2 ? 'navy':'#0081ff';
     loadBlinkCount++;
 }
+
 setInterval(loadBlink, 5000);
 Tone.loaded().then(start);
 
-backdrop.loop = true;
-backdrop.volume.value = '-12';
-player.connect(echo);
 
 const newCaption = ["00:00:06.420,00:00:08.160\nwhere is the water's edge?", '00:00:11.700,00:00:13.380\nin the wide expanses', '00:00:15.030,00:00:16.950\nor the half filled bathtub', '00:00:19.260,00:00:20.100\nin the river', '00:00:20.580,00:00:21.840\nor the boiling kettle', '00:00:22.770,00:00:23.760\nin the steam', '00:00:23.880,00:00:24.750\nor the snow', '00:00:25.140,00:00:28.170\nor the rainbow mist of a summer hose'];
 
 let captionObject = syncCC.splitCaptions(newCaption);
 let playFlag = false;
-
-// let justText = captionObject.map(text => text.content[0]);
 
 function loaded(){
         // todo: add load status message for screen readers
@@ -39,10 +38,12 @@ function loaded(){
         });
 }
 
+/**
+ * 
+ * @param {Object} captionObject  
+ */
 
-function start(){
-
-    loaded();
+function placeCaptions(captionObject){
     captionObject.forEach((text,i) => {
         let textElement = document.createElement('div');
         let colourValue = ((12+i)*10)+(Math.random()*(255-((12+i)*10)));
@@ -65,6 +66,11 @@ function start(){
         captionObject.element = textElement;
         document.body.appendChild(textElement);
     });
+}
+
+function start(){
+    loaded();
+    placeCaptions(captionObject);
     
     touch.setAction('.text');
     
