9325495606e58fbd766da3287e09200e1c373682
Charles Matthews <charles0matthews@gmail.com>
2023-09-12 17:28:03 -0400

    Update backButton.js

--- src/js/backButton.js
+++ src/js/backButton.js
@@ -1,25 +1,19 @@
 function addBackButton(){
     let urlParams = new URLSearchParams(window.location.search);
     
-    if(urlParams.has('from')){
-        let home = urlParams.get('from');
-    }
-    
-    if(urlParams.has('standalone') || document.querySelector(".nav__back")){
-        return;
-    }
+    if(urlParams.has('standalone') || document.querySelector(".nav__back")) return;
     
     let homePage = urlParams.get('from') == 'map' ? 'map2' : 'index';
     let backButton = document.createElement("button");
     
     backButton.innerHTML = "Biodivergent Sites and Sounds"; 
+    backButton.id = "backButton";
+    backButton.classList.add('button', 'nav__back', 'allowDefault')
     
     backButton.addEventListener('click',()=>{
         window.location = `https://elinorrowlands.github.io/bss/${homePage}.html`
     });
     
-    backButton.id = "backButton";
-    backButton.classList.add('button', 'nav__back', 'allowDefault')
     document.body.appendChild(backButton);
 }
 
