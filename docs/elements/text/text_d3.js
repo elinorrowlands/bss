863aaf1d6c9d417f7aee544532520b23b848b48e
Charles Matthews <charles0matthews@gmail.com>
2023-09-28 16:19:05 -0400

    text demo coming together, new demo in ripples folder

--- docs/elements/text/text_d3.js
+++ docs/elements/text/text_d3.js
@@ -1,9 +1,19 @@
-let   w = 1000,
-      h =  800,
-      circleWidth = 5; 
+// let   w = 1000,
+//       h =  800,
+//       circleWidth = 5; 
  
 let textInput = 'the quick brown fox jumps over the lazy dog';
 textInput = textInput.split(' ');
+
+textInput= 
+    `where is the water's edge? 
+    \nin the wide expanses? 
+    \nor the half filled bathtub? 
+    \nin the river? 
+    \nor the boiling kettle?
+    \nin the steam? 
+    \nor the snow?
+    \nor the rainbow mist of a summer hose?`.split('\n');
 
 let items = [];
 textInput.forEach((entry,i)=>{
@@ -13,46 +23,66 @@ textInput.forEach((entry,i)=>{
     })
 })
 
-let svgElement = d3.select('body')
+let container = d3.select('body')
     .append("div")
-    .classed("svg-container", true)
-    .append('svg')
-    .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", `0 0 ${window.innerWidth} ${window.innerHeight}`)
+    .classed("wordContainer", true)
+    
 
 let force = d3.layout.force()
     .nodes(items)
-    .gravity(0.5)
+    .gravity(0.1)
     .charge(-1000)
-    .size([window.innerWidth,window.innerHeight]); 
+    .size([window.innerWidth*0.75,window.innerHeight*0.75]); 
 
-let item = svgElement.selectAll('circle') 
+let item = container.selectAll('wordContainer') 
     .data(items).enter() 
-    .append('g') 
-    .call(force.drag); 
-
-item.append('circle')
-    .attr('cx', d=>d.x)
-    .attr('cy', d=>d.y)
-    .attr('r', (d,i)=>circleWidth + d.size)
-    .classed('transparent', true)
+    .append('div')
     .classed('words', true)
+    .classed('text', true)
+    .classed('moving', true)
+    .call(force.drag); 
+    // .append('g') 
+
+// item.append('svg')
+//     .attr("preserveAspectRatio", "xMinYMin meet")
+//     .attr("viewBox", (d,i)=>{
+//         let dim = circleWidth + d.size;
+//         return `0 0 ${dim} ${dim}`
+//     })
+//     .attr("position", "absolute")
+//     .attr('top', d=>d.x)
+//     .attr('left', d=>d.y)
+//     .append('circle')
+//     .attr(cx,(d,i)=>{
+//         let dim = circleWidth + d.size;
+//         return dim
+//     })
+//     .attr('r', (d,i)=>{
+//         let dim = circleWidth + d.size;
+//         return dim
+//     })
+//     .classed('transparent', true)
+//     .classed('words', true)
           
    // setInterval(function(){force.alpha(Math.random());},1250);
-setInterval(()=>{force.alpha(0.01)},250);
+
+   // setInterval(()=>{force.alpha(0.01)},250);
 
       
-force.on('tick', (e)=>{ 
-    item.attr('transform', function(d, i){
-        return 'translate(' + d.x + ','+ d.y + ')'
-    })
+force.on('tick', function(e){ 
+    // item.attr('transform', function(d, i){
+    //     return 'translate(' + d.x + ','+ d.y + ')'
+    // })
+    item.style('top', d=>{return `${parseInt(d.y)}px`})
+        .style('left', d=>{return `${parseInt(d.x)}px`})
+        // .style('font-size', d=>{return `${d.size * 1.5}px`})
 });
 
 
-item.append('text')
+item.append('div')
     .text(d=>d.name)
-    .classed('words', true)
-    .classed('text', true)
+    // .classed('5ords', true)
+    // .classed('text', true)
 
 force.start();
 
