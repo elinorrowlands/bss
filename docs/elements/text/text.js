0f47cb657c073526a73265b94deb73d3d30e1645
Charles Matthews <charles0matthews@gmail.com>
2023-09-28 12:09:14 -0400

    updating text/words element

--- docs/elements/text/text.js
+++ docs/elements/text/text.js
@@ -39,6 +39,7 @@ let captionObject = syncCC.splitCaptions(newCaption);
 window.playFlag = false;
 
 function startElement(){
+    console.log('start')
     document.querySelector('section.instructions').style.display = 'none';
     Tone.loaded().then(start);
 }
