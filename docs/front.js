fb4d2d0c985ba8c4a2cc6a1dbf983a8bab439dba
Charles Matthews <charles0matthews@gmail.com>
2023-09-30 10:08:13 -0400

    front page object/svg, script to file

---
+++ docs/front.js
@@ -0,0 +1,18 @@
+/**
+ * Temporary fix until I have time to redo the layout.
+ */
+
+function forceIconLocation(){
+    let infoIcon = document.querySelector('.icon__info');
+    let rect = infoIcon.getBoundingClientRect();
+    let footer = document.querySelector('footer');
+    let footerRect = footer.getBoundingClientRect();
+    if(rect.bottom > footerRect.top){
+        infoIcon.style.transform = `translateY(${-100}px)`;
+    }
+}
+
+window.addEventListener('load',()=>{
+    forceIconLocation();
+})
+
