8bf0f5784953e9ee28871d939bda20dcac3af819
Charles Matthews <charles0matthews@gmail.com>
2023-10-04 20:54:16 -0400

    abstracting sampler

--- docs/elements/heart/sampler.js
+++ docs/elements/heart/sampler.js
@@ -1,33 +1,34 @@
 let numberOfPlayers = 8;
 let sample = './harlesden1.mp3';
-window.players = [];
-window.playCount = 0;
 
-function spreadAcrossSample(query = 'image', players = window.players){
+
+const calculateStartInterval = function(query = 'image', players = window.players, index = 0){
     let divisions = document.querySelectorAll(query).length;
-    let allocations = [];
-    for(let i = 0; i < divisions; i++){
-        allocations.push(-1);
-    }
-    players.forEach((player, i)=>{
-        let allocation = Math.floor(Math.random()*divisions);
-        while(allocations[allocation] != -1){
-            allocation = Math.floor(Math.random()*divisions);
-        }
-        allocations[allocation] = i;
-        player.allocation = allocation;
-    })
+    let lengthInSeconds = players[0].player.buffer.duration;
+    return lengthInSeconds/divisions * index;
 }
 
+const getLowestPlayNumber = (players)=>{
+   return players.reduce((lowestPlayer, currentPlayer) => {
+        if (currentPlayer.playNumber < lowestPlayer.playNumber) {
+            return currentPlayer;
+        } else {
+            return lowestPlayer;
+        }
+    }, players[0]);
+}
 
-window.addEventListener('load',()=>{
+function initSampler(sample, numberOfPlayers = 6, distribute = calculateStartInterval){
+    window.players = [];
+    window.playCount = 0;
+    
     for(let i = 0; i < numberOfPlayers; i++){
         players.push({
             player: new Tone.Player(sample).toDestination(),
             allocation: -1,
             playNumber:0
         });
-        // for some reason setting the volume to zero on load doesn't work
+        // for some reason, setting the volume to zero on load doesn't work
         // players[i].volume.rampTo(-Infinity, 0.1)
         players[i].player.connect(echo);
     }
@@ -36,29 +37,21 @@ window.addEventListener('load',()=>{
     window.addEventListener('touch-pickup', (e)=>{
         let {x,y, element, type} = e.detail;
         let id = parseInt(element.id.split('_')[1]);
-        if(isNaN(id)){
-            return;
-        }
+        if(isNaN(id)) return;
         
         if(type == 'start' || type == 'enter'){
-            window.lengthInSeconds = players[0].player.buffer.duration;
+            
             let nextAvailablePlayer = players.map(player=>player.player.state).indexOf('stopped');
             
             playCount++;
             if(!players[nextAvailablePlayer]) {
-                const lowestPlayNumberPlayer = players.reduce((lowestPlayer, currentPlayer) => {
-                    if (currentPlayer.playNumber < lowestPlayer.playNumber) {
-                        return currentPlayer;
-                    } else {
-                        return lowestPlayer;
-                    }
-                }, players[0]);
+                let lowestPlayNumberPlayer = getLowestPlayNumber(players);
                 nextAvailablePlayer = players.indexOf(lowestPlayNumberPlayer);
             };
             
             players[nextAvailablePlayer].playNumber = playCount;
             players[nextAvailablePlayer].player.volume.rampTo(-6, 0.01);
-            players[nextAvailablePlayer].player.start(Tone.now(), (id/divisions)*lengthInSeconds*0.9);
+            players[nextAvailablePlayer].player.start(Tone.now(), distribute('image', players, id));
             players[nextAvailablePlayer].allocation = id;
             
         } else if (type == 'end' || type == 'leave'){
@@ -71,6 +64,10 @@ window.addEventListener('load',()=>{
             
         }
     })
+}
+
+window.addEventListener('load',()=>{
+    initSampler(sample, numberOfPlayers, calculateStartInterval)
     
 })
 
