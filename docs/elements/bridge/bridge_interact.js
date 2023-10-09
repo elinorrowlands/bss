2d4918e8caea7e30428de22056bdfb55650b469b
Charles Matthews <charles0matthews@gmail.com>
2023-10-09 19:34:00 -0400

    editing beyond element

--- docs/elements/bridge/bridge_interact.js
+++ docs/elements/bridge/bridge_interact.js
@@ -27,57 +27,88 @@ window.addEventListener('load',()=>{
         document.querySelector('.vectorised__container').appendChild(container);
     })
     
+    document.querySelectorAll('.beyond.photo').forEach(vectorImage=>{
+        let container = getContainerSvg(vectorImage);
+        container.classList.add('beyond__parent');
+        document.querySelector('.beyond__container').appendChild(container);
+    })
+    
     document.querySelectorAll('.vectorised.bridge__foreground').forEach(vectorImage=>{
         let container = getContainerSvg(vectorImage);
         container.classList.add('vectorised__parent');
         document.querySelector('.bridge__container').appendChild(container);
     })
     
+    document.querySelector('main').appendChild(document.querySelector('.beyond__container'));
     document.querySelector('main').appendChild(document.querySelector('.vectorised__container'));
     document.querySelector('main').appendChild(document.querySelector('.bridge__container'));
     document.querySelector('main').replaceWith(document.querySelector('main'))
     
     touch.setAction('.interact');
     
-    window.addEventListener('touch-pickup',e=>{
-        let {element, type}=e.detail;
-        console.log(e.detail)
-        document.querySelectorAll('.vectorised__container').forEach(container=>{   
-            // container.classList.add('vectorised__parent')
-            // console.log(vectorImage.parentElement)
-            if(type=='start'||type=='enter'){
-                container.style.opacity=0.9;
-            } else if(type=='end'||type=='leave'){
-                container.style.opacity=0.1;
-            }
-            
-        })
-        
-        document.querySelectorAll('.bridge__container').forEach(container=>{   
-            // container.classList.add('vectorised__parent')
-            // console.log(vectorImage.parentElement)
-            if(type=='start'||type=='enter'){
+    window.addEventListener('touch-pickup', e => {
+        let { element, type, x,y } = e.detail;
+
+        if (type == 'start' || type == 'enter') {
+            document.querySelectorAll('.vectorised__container').forEach(container => {
+                container.style.opacity = 0.8;
+            });
+
+            document.querySelectorAll('.bridge__container').forEach(container => {
                 container.style.opacity = 0.9;
-                container.style.filter='brightness(10%)';
-            } else if(type=='end'||type=='leave'){
+                container.style.filter = 'brightness(10%)';
+            });
+
+            document.querySelectorAll('.photo__container').forEach(container => {
                 container.style.opacity = 0.1;
-                container.style.filter='brightness(100%)';
+            });
+            if(element.id == 'bridge__beyond') {
+                document.querySelectorAll('.beyond__container').forEach(container => {
+                    container.style.opacity = 0.9;
+                    container.style.filter = 'sepia(100%)';
+                });
             }
-            
-        })
-        
-        document.querySelectorAll('.photo__container').forEach(container=>{   
-            // container.classList.add('vectorised__parent')
-            // console.log(vectorImage.parentElement)
-            if(type=='start'||type=='enter'){
-                container.style.opacity=0.1;
-            } else if(type=='end'||type=='leave'){
-                container.style.opacity=1;
+        } else if (type == 'end' || type == 'leave') {
+            document.querySelectorAll('.vectorised__container').forEach(container => {
+                container.style.opacity = 0.1;
+            });
+
+            document.querySelectorAll('.bridge__container').forEach(container => {
+                container.style.opacity = 0.1;
+                container.style.filter = 'brightness(100%)';
+            });
+
+            document.querySelectorAll('.photo__container').forEach(container => {
+                container.style.opacity = 1;
+            });
+            if(element.id == 'bridge__beyond') {
+                document.querySelectorAll('.beyond__container').forEach(container => {
+                    container.style.opacity = 0.9;
+                    container.style.filter = 'sepia(0%)';
+                });
             }
-            
-        })
-        
-    })
+        } else if(type=='move'){
+            if(element.id == 'bridge__beyond') {
+                document.querySelectorAll('.beyond__container').forEach(container => {
+                    container.style.opacity = 0.9;
+                    container.style.filter = 'sepia(100%)';
+                });
+            }
+            console.log('move',x,y, e.detail)
+            document.querySelectorAll('.vectorised__container').forEach(container => {
+                container.style.opacity = 0.9;
+            });
+
+            document.querySelectorAll('.bridge__container').forEach(container => {
+                container.style.opacity = 0.9;
+                container.style.filter = 'brightness(10%)';
+            });
+
+            document.querySelectorAll('.photo__container').forEach(container => {
+                container.style.opacity = 0.1;
+            });
+        }
+    });
     
 })
 
