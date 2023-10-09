16710bea2d560b9e5bc55b43472f91f264d61d90
Charles Matthews <charles0matthews@gmail.com>
2023-10-09 10:38:42 -0400

    experimenting, identify filter issues in safari

--- docs/elements/bridge/bridge_interact.js
+++ docs/elements/bridge/bridge_interact.js
@@ -1,19 +1,25 @@
-document.querySelectorAll('image').forEach((x,i)=>{
-    if(i%2===0) return;
-    x.style.filter=`invert(${i*2}%) sepia(${i*2%100}%) saturate(4212%) hue-rotate(164deg) brightness(98%) contrast(103%)`
-})
-
-touch.setAction('.interact');
-
-window.addEventListener('touch-pickup',e=>{
-    let {element, type}=e.detail;
-    console.log(e.detail)
-    document.querySelectorAll('.vectorised').forEach(vectorImage=>{
-        if(type=='start'||type=='enter'){
-            vectorImage.style.opacity=0.9;    
-        } else if(type=='end'||type=='leave'){
-            vectorImage.style.opacity=0.1;
-        }
-        
+window.addEventListener('load',()=>{
+    document.querySelectorAll('image').forEach((x,i)=>{
+        // if(i%2===0) return;
+        x.style.filter=`invert(${i*2}%) sepia(${i*2%100}%) saturate(4212%) hue-rotate(164deg) brightness(98%) contrast(103%)`
     })
+    
+    document.querySelector('.split.trees').style.filter = `invert(${0}%) sepia(${55}%) saturate(4212%) hue-rotate(104deg) brightness(98%) contrast(103%)`
+    
+    touch.setAction('.interact');
+    
+    window.addEventListener('touch-pickup',e=>{
+        let {element, type}=e.detail;
+        console.log(e.detail)
+        document.querySelectorAll('.vectorised').forEach(vectorImage=>{
+            if(type=='start'||type=='enter'){
+                vectorImage.style.opacity=0.9;    
+            } else if(type=='end'||type=='leave'){
+                vectorImage.style.opacity=0.1;
+            }
+            
+        })
+    })
+    
 })
+
