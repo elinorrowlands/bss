7e586871a98a5b7c454db426404543bbf8e2fa1f
Charles Matthews <charles0matthews@gmail.com>
2023-10-09 10:18:27 -0400

    testing bridge svg

--- docs/elements/bridge/bridge_interact.js
+++ docs/elements/bridge/bridge_interact.js
@@ -1,4 +1,12 @@
 document.querySelectorAll('image').forEach((x,i)=>{
-    if(i%2===1) return;
+    if(i%2===0) return;
     x.style.filter=`invert(${i*2}%) sepia(${i*2%100}%) saturate(4212%) hue-rotate(164deg) brightness(98%) contrast(103%)`
+})
+
+touch.setAction('.interact');
+
+window.addEventListener('touch-pickup',e=>{
+    let {element}=e.detail;
+    console.log(e.detail)
+    
 })
