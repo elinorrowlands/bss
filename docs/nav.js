8cc1aaca15401c4afa2179060e821656b016112e
Charles Matthews <charles0matthews@gmail.com>
2023-10-16 22:14:29 -0400

    temporary fix for navigation

---
+++ docs/nav.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', ()=>{
+    console.log('loaded')
+            //- document.querySelector('.highlight').scrollIntoView();
+            document.querySelectorAll('.nav__back').forEach((el)=>{
+                console.log(el)
+                el.addEventListener('click', ()=>{
+                    window.location.href="index.html";
+                })
+            })
+        })
