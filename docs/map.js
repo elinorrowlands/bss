7879eba9e8e86061a4d6adba204223398c329a50
Charles Matthews <charles0matthews@gmail.com>
2023-10-20 16:10:50 -0400

    basic map interaction

--- docs/map.js
+++ docs/map.js
@@ -1,5 +1,35 @@
- window.addEventListener('load',()=>{
-     window.addEventListener('touch-pickup',(e)=>{console.log(e.detail)});
+function positionNumbers(){
+    document.querySelectorAll('.element__marker').forEach(x=>{
+        let number = document.querySelector(`.${x.id}`);
+        x.dataset.number = number.innerHTML;
+        let pos=[x.getAttribute('cx'),x.getAttribute('cy')]
+        number.setAttribute('transform',`translate(${pos[0]} ${pos[1]})`)
+    })
+}
+
+let elements = {
+    "1": {name:"bridge", folder:"bridge"},
+    "2": {name:"water's edge", folder:"text"},
+    "3": {name:"crossing", folder:"crossing"},
+    "4": {name:"ripples", folder:"ripples"},
+    "5": {name:"rusty can", folder:"rusty_can"},
+    "6": {name:"school", folder:"school"}
+}
+
+const handleMarker = (e)=>{
+    let {element, type} = e.detail;
+    console.log(elements[element.dataset.number])
+    if(type=="end") window.location.href=`elements/${elements[element.dataset.number].folder}/index.html?from=map`;
+}
+
+
+window.addEventListener('load',()=>{
+    
+    positionNumbers();
+    
+     window.addEventListener('touch-pickup', handleMarker);
      window.touch.setAction('.element__marker');
      window.touch.listen();
+     
+     
 })
