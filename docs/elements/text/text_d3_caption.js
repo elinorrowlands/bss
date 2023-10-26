25c0931d562a49d4aed73f4c3a867aa0c8782c72
Charles Matthews <charles0matthews@gmail.com>
2023-10-26 12:35:28 -0400

    updates to text demo

--- docs/elements/text/text_d3_caption.js
+++ docs/elements/text/text_d3_caption.js
@@ -38,58 +38,62 @@ export default function generateBlocks(text){
     let force = d3.layout.force()
     .nodes(items)
     .gravity(0.01)
-    .charge(-1000)
+    .charge(-300)
     .size([window.innerWidth*0.75,window.innerHeight*0.75]); 
 
     let item = container.selectAll('wordContainer') 
-    .data(items).enter() 
-    .append('div')
-    .classed('words', true)
-    .classed('text', true)
-    .classed('moving', true)
-    .classed('d3', true)
-    .call(force.drag); 
+        .data(items).enter() 
+        .append('div')
+        .classed('words', true)
+        .classed('text', true)
+        .classed('moving', true)
+        .classed('d3', true)
+        // .style('top', '0px')
+        .call(force.drag); 
     
     document.querySelectorAll('.words.text.d3').forEach((element,i) => {
         element.id = `text_${i}`;
+        // element.style.top = `${i/items.length*window.innerHeight/2}px`;
+        // element.style.left = `10px`
     })
     window.gravity=0.01
     window.addEventListener('bump', (e) => {
-    // was 0.5
-    let {value} = e.detail; 
-    force.alpha(value);
-    force.charge(-1000*value);
-    window.gravity+=0.001;
-    if(window.gravity>0.1) window.gravity=0.01;
-    console.log('gravity', window.gravity)
-    force.gravity(window.gravity)
-    if(window.rippleScene){
-        rippleScene.ripples.mouse.velocity = {x:1, y:1};
-    }
+        // was 0.5
+        let {value} = e.detail; 
+        force.alpha(value);
+        force.charge(-1000*value);
+        window.gravity+=0.001;
+        if(window.gravity>0.1) window.gravity=0.01;
+        console.log('gravity', window.gravity)
+        force.gravity(window.gravity)
+        if(window.rippleScene){
+            rippleScene.ripples.mouse.velocity = {x:1, y:1};
+        }
     });
     
     force.on('tick', function(e){ 
     // item.attr('transform', function(d, i){
     //     return 'translate(' + d.x + ','+ d.y + ')'
     // })
-    item.style('top', d=>{
-        let y = d.y;
-        if(y>window.innerHeight-50) y = window.innerHeight-50;
-        if(y<50) y = 50;
-        return `${parseInt(y)}px`
-    })
-        .style('left', d=>{
-            let x = d.x;
-            if(x>window.innerWidth-250) x = window.innerWidth-250;
-            if(x<50) x = 50;
-            return `${parseInt(x)}px`})
-        // .style('font-size', d=>{return `${(d.y/window.innerHeight*30)+10}px`})
+        item.style('top', d=>{
+            let y = d.y;
+            if(y>window.innerHeight-50) y = window.innerHeight-50;
+            if(y<50) y = 50;
+            return `${parseInt(y)}px`
+        })
+            .style('left', d=>{
+                let x = d.x;
+                if(x>window.innerWidth-250) x = window.innerWidth-250;
+                if(x<50) x = 50;
+                return `${parseInt(x)}px`})
+            // .style('font-size', d=>{return `${(d.y/window.innerHeight*30)+10}px`})
         // .style('font-size', d=>{return `${d.size * 1.5}px`})
     });
 
 
     item.append('span')
-    .text(d=>d.name)
+        .text(d=>d.name)
+        // .style('top', '0px')
     // .classed('words', true)
     // .classed('text', true)
 
