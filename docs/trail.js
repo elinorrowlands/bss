d6ef9f3ab6bd919f9cb377a8b76dfcb40c8139c5
Charles Matthews <charles0matthews@gmail.com>
2023-11-14 20:24:28 -0500

    Update trail.js

--- docs/trail.js
+++ docs/trail.js
@@ -5,13 +5,15 @@ window.addEventListener('load', () => {
     console.log(lengthOfPath)
     let count = 0;
     let positions = {start:1045, end:1750}
+    let length = (positions.end-positions.start);
+    window.soundDuration = 0;
     for(let i = 0; i < lengthOfPath; i++){
         if(i%5==0&&i>positions.start && i<positions.end){
             let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
             circle.setAttribute("cx", guide.getPointAtLength(i).x);
             circle.setAttribute("cy", guide.getPointAtLength(i).y-5);
             circle.setAttribute("r", Math.abs(Math.sin((i/20)+0.4)*5)+5);
-            circle.setAttribute("id", `${count}_${i}`);
+            circle.setAttribute("id", `guide__${count}_${i}`);
             circle.setAttribute("stroke", `rgba(${count%255}, 0, 255, 0.4)`);
             circle.classList.add('guide__circle');
             document.querySelector('.guide').appendChild(circle);
@@ -29,9 +31,10 @@ window.addEventListener('load', () => {
                 widget.getPosition(function(position) {
                     
                         widget.getDuration(function(duration) {
+                            window.soundDuration = duration;
                             percent = position * 100 / duration;
                         //   console.log(position, percent);
-                        let length = (positions.end-positions.start);
+                        
                             document.querySelector('#cursor_back').setAttribute('transform',`translate(${guide.getPointAtLength(positions.start+(percent/100*length)).x} ${guide.getPointAtLength(positions.start+(percent/100*length)).y})`)
                             document.querySelector('#cursor').setAttribute('transform',`translate(${guide.getPointAtLength(positions.start+(percent/100*length)).x} ${guide.getPointAtLength(positions.start+(percent/100*length)).y})`)
                             document.querySelectorAll('.guide circle').forEach((x,i)=>{
@@ -45,6 +48,24 @@ window.addEventListener('load', () => {
         });
        })
     },500); //Interval in ms  
+    
+    touch.setAction('.guide__circle')
+        .setAction('.element__marker')
+        .listen();
+    window.addEventListener('touch-pickup', e=>{
+        let { query, id } = e.detail;
+        console.log(query)
+        if(id == 'element__4'){
+            widget.play();
+        } else if(id=='element__6'){
+            widget.pause();
+        }else if(query == '.guide__circle'){
+            let idNumber = id.split('_')[2];
+            // console.log(idNumber, id/count)
+            widget.seekTo(idNumber/count*window.soundDuration);
+        }
+       
+    })
     
 });
 
