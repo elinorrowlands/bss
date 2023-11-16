3600596a31e41d88fb917065088f1947caa919d1
Charles Matthews <charles0matthews@gmail.com>
2023-11-15 19:09:43 -0500

    tidying up and testing design

--- docs/trail.js
+++ docs/trail.js
@@ -2,11 +2,13 @@ window.addEventListener('load', () => {
     
     let guide = document.querySelector('.guide path');
     let lengthOfPath = guide.getTotalLength();
-    console.log(lengthOfPath)
+    // console.log(lengthOfPath)
+    
     let count = 0;
     let positions = {start:1045, end:1750}
     let length = (positions.end-positions.start);
     window.soundDuration = 0;
+    
     for(let i = 0; i < lengthOfPath; i++){
         if(i%5==0&&i>positions.start && i<positions.end){
             let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
@@ -17,51 +19,51 @@ window.addEventListener('load', () => {
             circle.setAttribute("stroke", `rgba(${count%255}, 0, 255, 0.4)`);
             circle.classList.add('guide__circle');
             document.querySelector('.guide').appendChild(circle);
-            console.log(count, guide.getPointAtLength(i).x, guide.getPointAtLength(i).y);
+            // console.log(count, guide.getPointAtLength(i).x, guide.getPointAtLength(i).y);
             count++;
         }
         
     }
-    
-    var iframeElement   = document.querySelector('iframe');
+    window.interval = 500;
+    let iframeElement   = document.querySelector('iframe');
     // var iframeElementID = iframeElement.id;
     window.widget       = SC.Widget(iframeElement);
     
     setInterval(function(){ 
-                widget.getPosition(function(position) {
-                    
-                        widget.getDuration(function(duration) {
-                            window.soundDuration = duration;
-                            percent = position * 100 / duration;
-                        //   console.log(position, percent);
-                        
-                            document.querySelector('#cursor_back').setAttribute('transform',`translate(${guide.getPointAtLength(positions.start+(percent/100*length)).x} ${guide.getPointAtLength(positions.start+(percent/100*length)).y})`)
-                            document.querySelector('#cursor').setAttribute('transform',`translate(${guide.getPointAtLength(positions.start+(percent/100*length)).x} ${guide.getPointAtLength(positions.start+(percent/100*length)).y})`)
-                            document.querySelectorAll('.guide circle').forEach((x,i)=>{
-                                if(percent/100*count>=i){
-                                        // console.log(x)
-                                        x.classList.add('on')
-                                } else {
-                                        x.classList.remove('on')
-                                }
-                            })
+        widget.getPosition(function(position) {
+            
+            widget.getDuration(function(duration) {
+                window.soundDuration = duration;
+                percent = position * 100 / duration;
+            //   console.log(position, percent);
+            
+                document.querySelector('#cursor_back').setAttribute('transform',`translate(${guide.getPointAtLength(positions.start+(percent/100*length)).x} ${guide.getPointAtLength(positions.start+(percent/100*length)).y})`)
+                document.querySelector('#cursor').setAttribute('transform',`translate(${guide.getPointAtLength(positions.start+(percent/100*length)).x} ${guide.getPointAtLength(positions.start+(percent/100*length)).y})`)
+                document.querySelectorAll('.guide circle').forEach((x,i)=>{
+                    if(percent/100*count>=i){
+                            // console.log(x)
+                            x.classList.add('on')
+                    } else {
+                            x.classList.remove('on')
+                    }
+                })
         });
        })
-    },500); //Interval in ms  
+    },window.interval); //Interval in ms  
     
     touch.setAction('.guide__circle')
         .setAction('.element__marker')
         .listen();
+        
     window.addEventListener('touch-pickup', e=>{
         let { query, id } = e.detail;
-        console.log(query)
+        // console.log(query)
         if(id == 'element__4'){
             widget.play();
         } else if(id=='element__6'){
             widget.pause();
         }else if(query == '.guide__circle'){
             let idNumber = id.split('_')[2];
-            // console.log(idNumber, id/count)
             widget.seekTo(idNumber/count*window.soundDuration);
         }
        
