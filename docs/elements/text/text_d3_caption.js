5da370d38da22d941e7ceffba0a9b4ebf99bfd8d
Charles Matthews <charles0matthews@gmail.com>
2024-08-15 17:26:16 -0400

    testing new mp3

--- docs/elements/text/text_d3_caption.js
+++ docs/elements/text/text_d3_caption.js
@@ -26,7 +26,21 @@ export default function generateBlocks(text){
     //     `where is the water's edge?\nin the wide expanses\nor the half filled bathtub\nin the river\nor the boiling kettle\nin the steam\nor the snow\nor the rainbow mist of a summer hose`
     //         .split('\n');
     
-    window.textInput = `I reach out towards the water’s edge.\nThe water here is so clear and open and honest\nOn the skin of the water,\na mirror of the city’s skyline.\nUnder the bridge,\na reflection of an otherworld.\nI reach out for the water’s edge\nIn the steam of my warming tea\nIn the mist of deep winters\nFalling snow,\nstreaming rivers,\nflooding banks\nI stand and look out to the water’s edge\nRepressed memories,\nhidden dreams\nBeyond there are the waves of crashing ocean\npolluted sea.`.split('\n');
+    // "00:00:00.401,00:00:13.000\nI reach out towards the water's edge.",
+    // "00:00:13.000,00:00:29.000\nThe water here is so clear and open and honest",
+    // "00:00:29.139,00:00:49.000\nOn the skin of the water,",
+    // "00:00:49.269,00:01:05.161\na mirror of the city’s skyline.",
+    // "00:01:05.161,00:01:24.999\nUnder the bridge, a reflection of an otherworld. ",
+    // "00:01:24.999,00:01:33.175\nI reach out for the water’s edge",
+    // "00:01:33.175,00:01:35.581\nIn the steam of my warming tea",
+    // "00:01:35.581,00:01:37.955\nIn the mist of deep winters", 
+    // "00:01:37.955,00:02:12.179\nFalling snow, streaming rivers, flooding banks",
+    // "00:02:12.179,00:02:21.899\nI stand and look out to the water’s edge",
+    // "00:02:21.899,00:02:29.538\nRepressed memories, hidden dreams",
+    // "00:02:29.538,00:03:38.000\nBeyond there are the waves of crashing ocean polluted sea."    
+    
+    window.textInput = `I reach out towards the water’s edge.\nThe water here is so clear and open and honest\nOn the skin of the water,\na mirror of the city’s skyline.\nUnder the bridge, a reflection of an otherworld.\nI reach out for the water’s edge\nIn the steam of my warming tea\nIn the mist of deep winters\nFalling snow,\nstreaming rivers,\nflooding banks\nI stand and look out to the water’s edge\nRepressed memories, hidden dreams\nBeyond there are the waves of crashing ocean polluted sea.`.split('\n');
+    
     
     function removeDuplicates(textInput){
         let unique = {};
